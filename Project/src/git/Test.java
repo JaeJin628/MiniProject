81ac9f08d9b636efa3139c2055cb1cf4018249aa
DESKTOP-10I9QOE\USER <gkgkvlfxhd@naver.com>
2021-01-20 08:54:28 +0900

    기초자료구조복습을 위한 몬스터잡기 코드

--- Project/src/git/Test.java
+++ Project/src/git/Test.java
@@ -6,27 +6,32 @@ public class Test {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int monster = 100;
-		int User = 200;
+		int monster = 400;
+		int User =0;
 		int choice = 0;
 		while(true) {
-			System.out.println("1.공격,2.방어,3.포션먹기");
+			System.out.println("User: 0.입장 1.몬스터소환,2.공격");
 			choice = sc.nextInt();
-			if(choice == 1) {
-				if(User>monster || monster==User ) {
-					monster-=10;
-					User-=5;
+			if(choice == 0) {
+				System.out.println("유저가 입장하셨습니다");
+			}else if(choice == 1) {
+				    monster = 400;
+				   System.out.println("몬스터가 생성되었습니다");
+				   System.out.println("몬스터HP:" + monster);
+		   }else if(choice == 2) {
+			   if(User<monster) {
+				    monster-=20;
+				    System.out.println("유저가 공격!!!!!");
+					System.out.println("몬스터HP:"+ monster);
+				}else if(monster == 0) {
+					System.out.println("몬스터가 죽었습니다");
+					break;
 				}
-			}else if(choice == 2) {
-				monster -=3;
-			}else if(choice == 3) {
-				monster +=5;
-			}
-			System.out.println("몬스터 Hp: "+ monster);
-			System.out.println("유저 HP: " + User);
-		}
 		
-
 	}
 
-}
+  }
+ }
+}	
+	
+	
