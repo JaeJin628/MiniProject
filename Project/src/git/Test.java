6814688f46b5e108d2471c980e5b064f084a6b70
DESKTOP-10I9QOE\USER <gkgkvlfxhd@naver.com>
2021-01-19 09:25:00 +0900

    몬스터잡기 미니플젝

--- Project/src/git/Test.java
+++ Project/src/git/Test.java
@@ -1,10 +1,31 @@
 package git;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hi");
+		Scanner sc = new Scanner(System.in);
+		int monster = 100;
+		int User = 200;
+		int choice = 0;
+		while(true) {
+			System.out.println("1.공격,2.방어,3.포션먹기");
+			choice = sc.nextInt();
+			if(choice == 1) {
+				if(User>monster || monster==User ) {
+					monster-=10;
+					User-=5;
+				}
+			}else if(choice == 2) {
+				monster -=3;
+			}else if(choice == 3) {
+				monster +=5;
+			}
+			System.out.println("몬스터 Hp: "+ monster);
+			System.out.println("유저 HP: " + User);
+		}
+		
 
 	}
 
